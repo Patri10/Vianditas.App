e755a6bbb8f5a60324cff36f8606f7820a1566f2
Patri4 <patrimendoza195@gmail.com>
2026-03-24 19:26:46 -0300

    First commit, arquitecture

---
+++ src/Vianditas.Domain/model/Usuarios.cs
@@ -0,0 +1,9 @@
+namespace Vianditas.Domain.model;
+public class Usuarios
+{
+    public int Id { get; set; }
+    public string ? Nombre { get; set; }
+    public string ? Correo { get; set; }
+    public string ? Contraseña { get; set; }
+}
+
